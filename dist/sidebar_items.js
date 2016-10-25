8c5b7c81eadfef6e6ed1a0597b5ae992507de5c0
Koziolek <bjkuczynski@gmail.com>
2016-10-25 22:06:43 +0200

    add documentation

---
+++ dist/sidebar_items.js
@@ -0,0 +1,1 @@
+sidebarNodes={"exceptions":[],"extras":[{"id":"api-reference","title":"API Reference","headers":[]}],"modules":[{"id":"Peselx","title":"Peselx","functions":[{"id":"validate/1","anchor":"validate/1"}]}],"protocols":[]}
