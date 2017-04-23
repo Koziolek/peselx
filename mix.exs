393ee5b559f50465212c462ea53f609dc788dbf3
Bartek Kuczynski <BKuczynski@luxoft.com>
2017-04-23 09:21:58 +0200

    Update minimum Elixir version to 1.4, now it is OK in all levels of config

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Peselx.Mixfile do
   def project do
     [app: :peselx,
      version: "0.2.1",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
